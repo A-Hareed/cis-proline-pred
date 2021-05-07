c2664db53d36ded4524727fafca5ce4141930a5f
Ayub's Linux computer <ayubm56@gmail.com>
2021-05-07 18:31:47 +0100

    updated torsion data to csv

---
+++ scripts/config.py
@@ -0,0 +1,6 @@
+#/usr/bin/python3
+
+prot_seq = {'ALA': 'a', 'ARG': 'r', 'ASN': 'n','ASP': 'd', 'CYS': 'c',
+            'GLU': 'e', 'GLN': 'q', 'GLY': 'g', 'HIS': 'h', 'ILE': 'i',
+            'LEU': 'l', 'LYS': 'k', 'MET': 'm', 'PHE': 'f', 'PRO': 'p',
+            'SER': 's', 'THR': 't', 'TRP': 'w', 'TYR': 'y', 'VAL': 'v'}
