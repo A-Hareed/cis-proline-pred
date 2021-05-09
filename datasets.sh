3a9f8bce2240cc9ebf81c0b3cee09aab03bed3c0
Ayub's Linux computer <ayubm56@gmail.com>
2021-05-09 13:35:59 +0100

    finished bash extraction script

--- datasets.sh
+++ datasets.sh
@@ -4,10 +4,26 @@
 cwd="$(pwd)/torsion"
 
 PDB="$(pwd)/pdb"
+#server pdb
+#goes here ....
+
+CSV="$(pwd)/data"
 
 script="$(pwd)/scripts"
-
+a=0
+#run pdbtorsions
+for file in $PDB/*.pdb
+do
+    a=$(($a+1))
+    if [ $a -eq 3 ]
+    then
+        echo "a = $a"
+        break
+    fi
+    pdbtorsions $file > $cwd/`basename $file .pdb`.txt.some
+done
+creat the csv output file 
 for file in $cwd/*.txt
 do
-    echo "$file" | $script/./CisProline_ext.py
+    echo "$file" | $script/./CisProline_ext.py >> $CSV/proline_dataset.csv
 done
