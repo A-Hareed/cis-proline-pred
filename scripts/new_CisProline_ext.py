21153e40f14eb301a6ffc0f7f897e9c91e7ab9d8
Ayub's Linux computer <ayubm56@gmail.com>
2021-05-24 14:22:25 +0100

    cleaning files

--- scripts/new_CisProline_ext.py
+++ scripts/new_CisProline_ext.py
@@ -3,7 +3,7 @@ import os
 import config # lookup table for single letter amino acid code
 import re
 
-os.chdir('/home/ayubh/project/git_clone/cis-proline-pred/torsion/') # changes directory
+os.chdir('/home/ayub/Desktop/cis_proline/cis-proline-pred/torsion') # changes directory
 
 file_names = os.listdir()  # gets file names ***if you whant to test with one torsion than use ['name'] 
 
@@ -17,7 +17,7 @@ for file in file_names: #loops through torsion files
         for i, tup in enumerate(torsion_list): #loop through list
             data_rows, form, no_flank, kmer = [], '', False, 3      #varibles and list set up
             if i > 1:
-                if tup[1] == "PRO" and (i + kmer) <= (len(torsion_list)-1) and (i - kmer) > 0:
+                if tup[1] == "PRO" and (i + kmer) <= (len(torsion_list)-1) and (i - kmer) >= 0:
                     kmer_range = range(1, (kmer+1)) # create range to extract flanking sequence
                     upstream_seq = [torsion_list[i + k][1] for k in kmer_range]
                     downstream_seq = [torsion_list[i - k][1] for k in reversed(kmer_range)]
@@ -26,10 +26,10 @@ for file in file_names: #loops through torsion files
                         aa_test1 = [False if aa not in config.prot_seq.keys() else True for aa in  upstream_seq]
                         aa_test2 = [False if aa not in config.prot_seq.keys() else True for aa in  downstream_seq]
                     # get torsion angle
-                    angle = abs(float(torsion_list[i-1][-1]))
+                    angle = abs(float(tup[-1]))
                     # regex for pdb name 
                     pdb_match = re.search(r'[/a-zA-Z_-]*([a-zA-Z0-9\_]+)\.', file) 
-                    pdb_name = pdb_match.group(1)
+                    pdb_name = pdb_match.group(1)   
                     if angle > 90:
                         form = 'trans'
                     else:
