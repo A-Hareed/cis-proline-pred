6df17afd8ae08a792ce177558941103623b4a981
Ayub's Linux computer <ayubm56@gmail.com>
2021-09-01 05:47:58 +0100

    update

--- scripts/build_train_test_sets.py
+++ scripts/build_train_test_sets.py
@@ -1,315 +1,194 @@
 #! /usr/bin/python3
+#*************************************************************************
+#
+#   Program:    build_train_test_sets
+#   File:       build_train_test_sets.py
+#
+#   Version:    V1.0
+#   Date:       01.06.21
+#   Function:   takes proline datasets and splits it into training and testing sets by
+#               outputing undersampled training set for Random Forest and imbalanced 
+#               training set for XGBoost 
+#               
+#
+#   Author:     Ayub Hareed
+#
+#
+#   EMail:     ayubm56@gmail.com
+#
+#*************************************************************************
+#
+#   This program is not in the public domain, but it may be copied
+#   according to the conditions laid out in the accompanying file
+#   COPYING.DOC
+#
+#   The code may be modified as required, but any modifications must be
+#   documented so that the person responsible can be identified. If
+#   someone else breaks this code, I don't want to be blamed for code
+#   that does not work!
+#
+#   The code may not be sold commercially or included as part of a
+#   commercial product except as described in the file COPYING.DOC.
+#
+#*************************************************************************
+#
+#   Usage: used to split proline dataset into training and testing samples
+#
+#   commandline Example: ./build_train_test_sets.py [proline csv file] [location to save file]
+#   ======
+#
+#*************************************************************************
 
 
-# import libiries 
+# Import libraries
 import sys
 import random
+import pandas as pd
 
-#-----------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 def read_file(filename):
-    # dictionary to have pdb name plus atom number as keys
-    file_dict = {}
-    with open(filename, 'r') as f:
-        file_list = f.read().splitlines()
-    for line in file_list:
-        temp_lst = line.split(',')
-        key = temp_lst[0] + '_' + temp_lst[1]
-        value = temp_lst[2:]
-
-        # add line to dictionary
-        file_dict[key] = value
-
-    return (file_dict) #returns a dictionary
+    """
+    read in the proline csv file and converts it into pandas dataframe
 
 
-#-----------------------------------------------------------
-def header_input(filename):
-    with open(filename, 'r') as f:
-        result = f.read()
-    return (result)
+    Input:   (string) filename - a filename for the csv proline file
+    Returns: (DataFrame) df - a pandas dataframe where the first line
+             of the CSV file is the header
 
-def get_cisdataset(cis_dic, fold):
+    01.06.21 Original By: Ayub Hareed
+    """
+    df = pd.read_csv(filename)
+    return (df)
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+def get_train_test(df1, used_cis):
+    """
+    using pandas and a list that contains cis proline usage  
+    to split into training and testing set
+
+
+    Input:   (DataFrame) df1 - a pandas dataframe that consits of proline 
+                               dataset
+
+             (list) used_cis - a list that contains cis proline used to in 
+                               test sets 
+
+    Returns: (DataFrame) df_train - a pandas dataframe that contains training
+                                    datasets
+
+             (DataFrame) df_test - a pandas dataframe  that contains training
+                                   datasets
+
+    01.06.21 Original By: Ayub Hareed
     """
 
+    # remove non-feature and label columns
+    df = df1.drop(['pdb', 'atom num'], axis='columns')
+    cis = df[df['type'] == 'cis'] # asign cis samples to new variable 
+
+    # find the amount of cis to split inot five sets
+    cis_num = cis.type.value_counts()
+    num = int(int(cis_num[0])/5)
+    
+    print(f'\n\n#########################\ncis test length = {num}\n\n')
+    uncut_cis_full = cis.drop(used_cis)  # drop the used cis samples to avoid 
+                                         # cross-contamination
+
+    cis_test = uncut_cis_full.sample(num)  # randomly select "num" amount of cis
+
+    ct_index = cis_test.index.values.tolist()
+
+    cis_train = cis.drop(ct_index)  # drop the selected training cis
+
+
+    used_cis.extend(ct_index) # update the cis used list
+
+    # now do the trans dataset
+
+    trans = df[df['type'] == 'trans']
+
+    trans_test = trans.sample(num)  # randomly select "num" amount of trans
+    
+    tt_index = trans_test.index.values.tolist()
+
+    trans_train = trans.drop(tt_index)  # drop the selected training trans
+
+    # combine trans and cis dataframes
+    df_train = pd.concat([trans_train, cis_train], axis=0) 
+    
+    df_test = pd.concat([trans_test, cis_test], axis=0)
+
+    return (df_train, df_test, used_cis)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+def training_set(df):
     """
+    using training dataframe the trans samples are undersampled 
+    to use for Random Forest
+
+
+    Input:   (DataFrame) df - a pandas dataframe that consists of proline 
+                              training dataset
+
+
+    Returns: (DataFrame) df_final - a pandas dataframe that consits of cis
+                                    proline samples and undersampled trans
+                                    samples
+
+    01.06.21 Original By: Ayub Hareed
+    """
+
+    cis = df[df['type'] == 'cis']
+    trans = df[df['type'] == 'trans']
+    cis_num = cis.type.value_counts()
+    # gets number of cis samples 
+    sample_size = int(cis_num[0])
     
-    keys = cis_dic.keys()
-    pdb_lst = []
-    for i in keys:
-        split_point = i.rfind('_')
-        pdbname = i[:split_point]
-        pdb_lst.append(pdbname)
-    distinct_pdblst = set(pdb_lst)
-    amount_test = round((len(distinct_pdblst) * (fold/100)), 0)
-    amount_train = len(distinct_pdblst) - amount_test
-    # print(amount_test)
-    # create cis_test cis_testing_set
-    cis_test_pdbused = [] # used to keep used cis dataset to prevent duplications
-                          # between test sets
-    cis_test_lst = []
-    avalible_pdb = list(distinct_pdblst)
+    trans_training_set = trans.sample(sample_size)  # randomly samples trans using 
+                                                    # cis sample size
 
-    random.seed(43)
-    r = random.SystemRandom()
-    r.shuffle(avalible_pdb)
-    
-    for num in range(1,6):
-        temp_lst = []
-        # used to remove the used pdb
-        if (len(cis_test_pdbused) > 0):
-            for item in cis_test_pdbused:
-                avalible_pdb.remove(item)
-                
-            cis_test_pdbused = []
-        for i in range(int(amount_test)):
-            
-            if (i < len(avalible_pdb)):
-                pdb = avalible_pdb[i]
-                cis_test_pdbused.append(pdb)
-                for k, v in cis_dic.items():
+    # combine trans and cis samples to one dataframe                                                    
+    df_final = pd.concat([trans_training_set, cis], axis=0)
 
-                    underscr = k.rfind('_')
-                    key_prefix =  k[:underscr]
-                    key_suffix = k[(underscr+1):]
+    return (df_final)
 
-                    if (key_prefix == pdb):
-                        line = ''
-
-                        for n,data in enumerate(v):
-                            if (n == 0):
-                                line += key_prefix + ',' + key_suffix + ',' + data
-
-                            if ( n > 0 and   n < (len(v) -1)):
-                                line +=  ',' + data
-                                
-                            elif (n == (len(v) - 1)):
-                                line += ',' + data + '\n'
-                                
-                        temp_lst.append(line)
-
-            # else:
-                # print(f'{len(avalible_pdb)} {i}')
-        cis_test_lst.append(temp_lst)
-    
-    # create test dataset 
-    cis_training_lst = []
-    
-    for i in range(5):
-
-        # remove one batch and combine the remaining four
-        # test batches
-        # print (i)
-        temp_lst = []
-
-        for k in cis_test_lst:
-            if k != cis_test_lst[i]:
-                temp_lst.extend(k)
-        
-        # print(len(temp_lst))
-        cis_training_lst.append(temp_lst)
-    # print(cis_training_lst)   
-    set1 = set(cis_test_lst[0])
-    set2 = set(cis_test_lst[1])
-    c = set1.intersection(set2)
-
-    # print(cis_test_lst[0][-1])
-    # print(cis_test_lst[1][-1])
-    # print(f'the union between 1 and 2 {len(c)}')
-
-    return (cis_test_lst, cis_training_lst)
-
-#-----------------------------------------------------------
-def get_transdataset(trans_dic, num = 5):
-
-    amount_test = int(round((len(trans_dic.keys())) / 5, 0))
-    
-    # create a list of keys to tehn randomly shuffle for selection test selection
-    keys_lst = list(trans_dic.keys())
-    
-    random.seed(43)
-    r = random.SystemRandom()
-    r.shuffle(keys_lst)
-    # print(len(keys_lst))
-
-    # create testing trans dataset
-    trans_test_lst = []
-    trans_data_used = []
-    
-    for i in range(num):
-        temp_lst = []
-
-        if (len(trans_data_used) > 0):
-            for item in trans_data_used:
-                keys_lst.remove(item)
-            trans_data_used = []
-
-        for size in range(amount_test):
-            line = ''
-    
-            if (size < len(keys_lst)):
-                data_key = keys_lst[size]
-                trans_data_used.append(keys_lst[size])
-
-                data_value = trans_dic[data_key]
-
-                underscr = data_key.rfind('_')
-                key_prefix = data_key[:underscr]
-                key_suffix = data_key[(underscr+1):]
-                
-                for n, data in enumerate(data_value):
-                    if (n == 0):
-                        line += key_prefix + ',' + key_suffix + ',' + data
-                    
-                    if ( n > 0 and   n < (len(data_value) - 1)):
-                        line += ',' + data
-                    elif (n == (len(data_value) - 1)):
-                        line += ',' + data + '\n'
-            
-            temp_lst.append(line)
-        trans_test_lst.append(temp_lst)
-    
-    # create the training batchs
-    trans_training_lst = []
-    for i in range(5):
-        
-        # remove one batch and combine the remaining four
-        # test batches
-        # print(i)
-        temp_lst = []
-        
-        for j in range(5):
-            if (i != j):
-                temp_lst.extend(trans_test_lst[j])
-            
-
-            # temp_lst.remove(trans_test_lst[i])
-            # if k != trans_test_lst[i]:
-                # print(k)
-
-        # print(len(temp_lst), 'temp_lst')
-        trans_training_lst.append(temp_lst)
-    # print(cis_training_lst)
-    my_set1 = set(trans_test_lst[0])
-
-    my_set2 = set(trans_test_lst[1])
-    c = my_set1.intersection(my_set2)
-    # print(len(trans_test_lst))
-    # print('training')
-    # print(f'the union between 1 and 2 {len(c)}')
-    # print(trans_test_lst[0][-1])
-    # print(trans_test_lst[1][-1])
-
-    return (trans_test_lst, trans_training_lst)
-#------------------------------------------------------------------
-def combine_cis_trans(cis_lst, trans_lst, header, train=None):
-    result = ''
-    multi_cis = []
-    random.seed(43)
-    r = random.SystemRandom()
-
-    if (train != None):
-        cis_trans_lst = []
-
-        r.shuffle(trans_lst)
-
-
-
-
-        multi = int(train)
-        for i in range(multi):
-            multi_cis.extend(cis_lst)
-        cis_trans_lst.extend(multi_cis)
-        print(len(cis_trans_lst))
-        for i in range(len(cis_trans_lst)):
-            cis_trans_lst.append(trans_lst[i])
-        print(len(cis_trans_lst))
-        # cis_trans_lst.extend(trans_lst)
-
-
-        
-        r.shuffle(cis_trans_lst)
-        result += header
-        result += ' '.join(cis_trans_lst)
-
-    else:
-        cis_trans_lst = []
-        cis_trans_lst.extend(cis_lst)
-        cis_trans_lst.extend(trans_lst)
-
-        r.shuffle(cis_trans_lst)
-        result += header
-        result += ' '.join(cis_trans_lst)
-    
-    return (result)
-
-
-
-#------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
 #           main program
-#-----------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# takes in cis and trans files from commandline 
-cis_filename = sys.argv[1]
+# get proline dataset filename
+filename = sys.argv[1]
 
-fold = int(sys.argv[3])
-
-outputname = sys.argv[4]
-
-header = sys.argv[5]
-title = ''
-title += header_input(header)
-
-print(f'header is :{title}')
-
-trans_filename = sys.argv[2]
-
-# create dictionaries 
-cis_dict = read_file(cis_filename)
-trans_dict = read_file(trans_filename)
-cis_testing_set, cis_training_set = get_cisdataset(cis_dict, fold)
-trans_testing_set, trans_training_set = get_transdataset(trans_dict)
+# get location to save training and testing files
+if (len(sys.argv) > 2):
+    file_location = sys.argv[2]
+else:
+    file_location = ''
 
 
-
-x = 5 + (4*18)
-print(x)
-
-y = int(round(27816/1437, 0))
-print(y)
-print(f'cis test list batch sizes {len(cis_testing_set)}:-----------')
-for i, j in enumerate(cis_testing_set):
-    print(f'test batch {i+1} size: {len(j)}')
-
-print(f'cis training list batch sizes {len(cis_training_set)}:-----------')
-for i, j in enumerate(cis_training_set):
-    print(f'training batch {i+1} size: {len(j)}')
-
-print(f'trans test list batch sizes {len(trans_testing_set)}:-----------')
-for i, j in enumerate(trans_testing_set):
-    print(f'test batch {i+1} size: {len(j)}')
-
-print(f'trans training list batch sizes {len(trans_testing_set)}:-----------')
-for i, j in enumerate(trans_training_set):
-    print(f'training batch {i+1} size: {len(j)}')
+used_cis = []
+df = read_file(filename)
+df1 = df.dropna() # drop any rows with missing values
 
 
-# combine_cis_trans(cis_lst=cis_training_set[0], trans_lst=trans_training_set[0], header= title, train=9)
+for i in range(1, 6):
+    df_train, df_test, used_cis = get_train_test(df1, used_cis)
 
+    # export testing samples
+    test_location = file_location + '/test_' + str(i) + '.csv'
+    df_test.to_csv(f'{test_location}', index=False)
+    print(f'amount of cis used {len(used_cis)}')
 
+    # weka Random Forest 20 undersampled training sets
+    for t_num in range(1, 21):
+        df_final = training_set(df_train)
+        train_location = file_location + '/train_c' + str(i) + '_t' + str(t_num) + '.csv'
+        df_final.to_csv(f'{train_location}', index=False)
 
-for i in range(5):
-    out_file_test = outputname + '_test_' + str(i) + '.csv'
-    out_file_training = outputname + '_train_' + str(i) + '.csv'
-
-    combine_train = combine_cis_trans(cis_lst=cis_training_set[i], trans_lst= trans_training_set[i], header=title, train=4)
-    combine_test = combine_cis_trans(cis_lst=cis_testing_set[i], trans_lst= trans_testing_set[i], header= title)
-
-
-    with open(out_file_training, 'w') as f:
-        f.write(combine_train)
-
-    with open(out_file_test, 'w') as f:
-        f.write(combine_test)
-
-
+        if t_num == 20:
+            print(df_final.type.value_counts())
+    
+    # XGBoost training sets
+    train_location = file_location + '/xg_train_c' + str(i) + '.csv'
+    df_train.to_csv(f'{train_location}', index=False)
 
--- scripts/models/weka_RF/random_forest_model.sh
+++ scripts/models/weka_RF/random_forest_model.sh
@@ -1,39 +1,47 @@
 #! /usr/bin/bash
+#*************************************************************************
+#
+#   Program:    random_forest
+#   File:       random_forest_model.sh
+#
+#   Version:    V1.0
+#   Date:       18.05.21
+#   Function:   build and test Random Forest models using weka on
+#               proline datasets
+#*************************************************************************
 
-echo "start!!!"
-
-
-while getopts h:e: flag
-do
-    case "${flag}" in
-            h) help_input="RUN";;
-            e) encoder=${OPTARG};;
-
-          [?]) printf >&2 "###############################################"
-    esac
-done
-
+# set up weka first 
 # export WEKA=/nas/backup/work/newserver/bsmhome/zcbthhn/weka-3-8-3
-export WEKA=/home/ayub/Downloads/weka-3-8-5-azul-zulu-linux/weka-3-8-5
+# export WEKA=/home/ayub/Downloads/weka-3-8-5-azul-zulu-linux/weka-3-8-5
+# Insert weka path:
+export WEKA=
+
 export CLASSPATH="$WEKA/weka.jar"
 CLASSIFIER="weka.classifiers.trees.RandomForest"
 
-# head -n 1 ${encoder}_test_1.csv | awk -F',' '{for(i=2;i<=NF;++i)print $i}' > input.txt
 
+# get the feature headers from csv files inoder to make arff files
+head -n 1 test_1.csv | awk -F',' '{for(i=2;i<=NF;++i)print $i}' > input.txt
+
+
+# loop through the five sets of test files along with each of its 
+# corresponding 20 training sets
 for num in $(seq 1 5)
 do
-    file_num=`echo ${encoder}_test_${num}.csv`
+    file_num=`echo test_${num}.csv`
     echo "${file_num}" 
     wc -l ${file_num}
     test_name=`basename ${file_num} .csv`
     training_letter=`echo c${num}`
     echo "${training_letter}"
-    train=`echo ${encoder}_train_c${num}`
+    train=`echo train_c${num}`
     echo "${train}"
     echo "${train}" >> mcc_output.txt
+
     # extract testing arff file
     csv2arff -ni input.txt type ${file_num} >${test_name}.arff
 
+    # loop through training sets
     for file in $train*.csv
     do
         wc -l ${file}
@@ -45,7 +53,7 @@ do
         
         java $CLASSIFIER -I 500 -t $filename.arff -d RF500_${filename}.model > RF500_${filename}.out
         java $CLASSIFIER -T ${test_name}.arff -p 0 -l RF500_${filename}.model >predicted_with_RF500_${filename}.out
-	echo "filename = ${filename}"
+	    echo "filename = ${filename}"
 
     done 
 
